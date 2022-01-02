ce3538e3fe2105cc0f4f042d1faf5abd2eed1fb8
TOJINA JOSEPH <34956387+TojinaJoseph@users.noreply.github.com>
2022-01-02 21:48:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require ('path'); 
 const cors = require('cors');
 const bodyParser=require('body-parser');//changed(2)
+const port = process.env.PORT || 5000;
 const nav= [
     {
         link:"/books",
@@ -56,6 +57,6 @@ app.get('/',function(req,res){
 
 
 
-app.listen(5000,()=>{
+app.listen(port,()=>{
     console.log("Server Ready on 5000");//changed(5)
 });
